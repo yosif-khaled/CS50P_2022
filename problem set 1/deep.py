a262cfbb0a7a5727f2b3083f4f933300fc201f9c
yosif-khaled <yosif.khaledjm@gmail.com>
2022-09-16 00:39:03 +0200

    deep.py first assignment in the second problem set -- runs fine for the purposes of the course

---
+++ problem set 1/deep.py
@@ -0,0 +1,17 @@
+def main():
+    str_userInput = input(" Enter 42 for Yes Other for No.. \n$ ")
+    if isInputFortyTwo(str_userInput) is True:
+        print("Yes")
+    else:
+        print("No")
+
+def isInputFortyTwo(str_userInput):
+    str_userInput = str_userInput.lower().replace(" ", "")
+    print(str_userInput)
+    viableInputs =  ["42", "fortytwo", "forty-two"]
+    # isViable asks if is there any value in the userInput that matches a value in the viableInputs list
+    isViable = any(value in str_userInput for value in viableInputs)
+    return isViable
+
+while True:
+    main()
