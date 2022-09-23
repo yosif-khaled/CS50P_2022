c611d72862723214c948efb980b1e8d5f4d57763
yosif-khaled <yosif.khaledjm@gmail.com>
2022-09-23 13:52:36 +0200

    Fixed some issues

--- problem set 2/coke.py
+++ problem set 2/coke.py
@@ -3,7 +3,7 @@ def main():
     claculateChange()
 
 def promptUser():
-    int_input = int(input('Enter Coin: '))
+    int_input = int(input('Insert Coin: '))
     return int_input
 
 def verifyInput(int_userInput):
@@ -14,25 +14,41 @@ def verifyInput(int_userInput):
     else:
         return False
 
+def calculateAmountDue(amountDue, coinInserted):
+    amountDue = amountDue - coinInserted
+    return amountDue
+
+def calculateInsertedTotal(insertedTotal, coinInserted):
+    insertedTotal = insertedTotal + coinInserted
+    return insertedTotal
+
+
 # This function is a mess needs to be refactored
 def claculateChange():
-    totalPrice = 50
-    remainingCoins = 50
+    insertedTotal = 0
+    amountDue = 50
     numberOfTries = 0
-    while remainingCoins != 0:
-        coinValue = promptUser()
-        isViable = verifyInput(coinValue)
+    while amountDue != 0:
+        coinInserted = promptUser()
+        isViable = verifyInput(coinInserted)
         if isViable:
-            remainingCoins = remainingCoins - coinValue
-            totalValueAccepted = totalPrice - remainingCoins
-            print(f'Remaining Coins: {remainingCoins}')
+            amountDue = calculateAmountDue(amountDue, coinInserted)
+            insertedTotal  = calculateInsertedTotal(insertedTotal, coinInserted)
+            if insertedTotal > 50:
+                changeOwed = amountDue * - 1
+                print(f'Change Owed: {changeOwed}')
+                # Ending Loop and returning Change
+                break
+            else:
+                print(f'Amount Due: {amountDue}')
         else:
             numberOfTries += 1
             print('Not Viable')
 
+        # Ending The Loop and Returning The Total Value Inserted To User
         if numberOfTries == 3:
             print(f'You Failed To Enter The Correct Value For {numberOfTries}')
-            print(f'Here is Your ${totalValueAccepted}')
+            print(f'Here is Your ${insertedTotal}')
             break
 
 if __name__ == "__main__":
